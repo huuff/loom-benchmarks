2845890dc7fed852fe02d25ad442586ddcd2d224
Haf <haf@protonmail.ch>
2022-10-09 15:45:53 +0200

    openapi generation

--- mvc-client/build.gradle.kts
+++ mvc-client/build.gradle.kts
@@ -21,8 +21,36 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+
+    // OpenAPI copy-pasted
+    implementation("io.swagger:swagger-annotations:1.6.5")
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("io.gsonfire:gson-fire:1.8.5")
+    implementation("javax.ws.rs:jsr311-api:1.1.1")
+    implementation("javax.ws.rs:javax.ws.rs-api:2.1.1")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.3")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("jakarta.annotation:jakarta.annotation-api:1.3.5")
 }
 
-tasks.getByName<Test>("test") {
+tasks.test {
     useJUnitPlatform()
+}
+
+val apiGeneratedSources = "$buildDir/generated-sources/client"
+openApiGenerate {
+    generatorName.set("java")
+    inputSpec.set("$projectDir/src/main/resources/api.json")
+    outputDir.set(apiGeneratedSources)
+}
+
+tasks.compileJava {
+    dependsOn(tasks.openApiGenerate)
+}
+
+java {
+    sourceSets["main"].java.srcDir(file("$apiGeneratedSources/src/main/java"))
 }
